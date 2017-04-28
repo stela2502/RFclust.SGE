6e63cd614edeca11efd1ee3e17ded5ca02caa6b7
stefan <st.t.lang@gmx.de>
2017-04-28 14:32:19 +0200

    brute force updating all installed packages.

--- install.R
+++ install.R
@@ -1,7 +1,15 @@
+
 source("https://bioconductor.org/biocLite.R")
 biocLite()
+
+install.packages( 
+		lib  = lib <- .libPaths()[1],
+		pkgs = as.data.frame(installed.packages(lib), stringsAsFactors=FALSE)$Package,
+		type = 'source'
+)
+biocLite("BiocUpgrade") 
+
 if (!library("devtools", quietly = TRUE,logical.return=TRUE )) {
 	install.packages(c('devtools'),  repos='https://ftp.acc.umu.se/mirror/CRAN/')
 	library(devtools)
 }
-install()
