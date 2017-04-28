ec49c35e1b15d1f9375c3e1b8da3017e1c40cafb
stefan <st.t.lang@gmx.de>
2017-04-28 15:35:12 +0200

    Hopefully the last fix.

--- install.R
+++ install.R
@@ -1,15 +1,13 @@
-
-source("https://bioconductor.org/biocLite.R")
-useDevel(devel=TRUE)
-biocLite()
-
 install.packages( 
 		lib  = lib <- .libPaths()[1],
 		pkgs = as.data.frame(installed.packages(lib), stringsAsFactors=FALSE)$Package,
 		type = 'source',  
 		repos='https://ftp.acc.umu.se/mirror/CRAN/'
 )
-biocLite("BiocUpgrade") 
+
+source("https://bioconductor.org/biocLite.R")
+useDevel(devel=TRUE)
+biocLite(ask=FALSE)
 
 if (!library("devtools", quietly = TRUE,logical.return=TRUE )) {
 	install.packages(c('devtools'),  repos='https://ftp.acc.umu.se/mirror/CRAN/')
