ae9d5fca6fc3c07af477279020ddb9ce5d25e0fe
Stefan Lang <Stefan.Lang@med.lu.se>
2017-05-16 13:53:18 +0200

    Try block around the install.packages.

--- install.R
+++ install.R
@@ -1,9 +1,10 @@
+try ({
 install.packages( 
 		lib  = lib <- .libPaths()[1],
 		pkgs = as.data.frame(installed.packages(lib), stringsAsFactors=FALSE)$Package,
 		type = 'source',  
 		repos='https://ftp.acc.umu.se/mirror/CRAN/'
-)
+) } )
 
 source("https://bioconductor.org/biocLite.R")
 #useDevel(devel=TRUE)
