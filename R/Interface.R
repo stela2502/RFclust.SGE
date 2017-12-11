e83882a3ac8df45bd69288c70c72042fc9ad57ad
stefanlang <Stefan.Lang@med.lu.se>
2017-12-11 09:18:36 +0100

    Large datasets need the R option '--max-ppsize=500000' set to not throw 'Error: protect(): protection stack overflow', but rather die of an memory exceeded error.

--- R/Interface.R
+++ R/Interface.R
@@ -17,7 +17,11 @@ setGeneric('RFclust.SGE', ## Name
 			standardGeneric('RFclust.SGE') ## der Aufruf von standardGeneric sorgt für das Dispatching
 		}
 )
-
+#setMethod('RFclust.SGE', signature = c ('data.frame'),
+#		definition = function ( dat, ..., tmp.path='', email='', slices=32, SGE=FALSE, slurm=FALSE, name='RFclust', settings=list() ) {
+#			RFclust.SGE( as.matrix(dat), ..., tmp.path='', email='', slices=32, SGE=FALSE, slurm=FALSE, name='RFclust', settings=list() )
+#		}
+#)
 setMethod('RFclust.SGE', signature = c ('data.frame'),
 		definition = function ( dat, ..., tmp.path='', email='', slices=32, SGE=FALSE, slurm=FALSE, name='RFclust', settings=list() ) {
 			if ( tmp.path == '' ){
@@ -225,7 +229,7 @@ setMethod('writeRscript', signature = c ('RFclust.SGE'),
 							paste('release.lock("',Rdata,'")',sep='')
 					), con=fileConn )
 			close(fileConn)
-			cmd <- paste('R CMD BATCH --no-save --no-restore --no-readline --', rscript ) 
+			cmd <- paste('R CMD BATCH --no-save --no-restore --no-readline --max-ppsize=500000 --', rscript ) 
 			if ( run ) {
 				system( paste(cmd,"&" ) )
 			}
